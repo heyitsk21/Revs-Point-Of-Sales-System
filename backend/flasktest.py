d467ee4f76db6398b1dff268125e2a1aff1c2d47
jnuccitelli <jnuccitelli1@gmail.com>
2024-03-23 21:37:23 -0500

    beginning to set up the flask project package created

--- backend/flasktest.py
+++ backend/flasktest.py
@@ -1,3 +1,5 @@
+# THIS FILE IS ONLY FOR TESTING PURPOSES TO MAKE SURE FLASK HAS BEEN INSTALLED DO NOT DEVELOP IN THIS FILE
+
 from flask import Flask #you can set up a vritual enviroment if you want to deal with project dependacies ask Joseph for more details 
 
 app = Flask(__name__)
@@ -5,6 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return 'Hello, Flask!'
+#DO NOT DEVELOP IN THIS FILE
 
 if __name__ == '__main__':
     app.run(debug=True)
