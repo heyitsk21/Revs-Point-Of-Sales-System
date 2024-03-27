a3a8c4ebae7f301d085c4ad7be691d1a58b74342
heyitsk21 <122894491+heyitsk21@users.noreply.github.com>
2024-03-27 12:01:37 -0500

    ordering trends done! all manager reports from project 2 are complete!

--- backend/revspos/manager_routes.py
+++ backend/revspos/manager_routes.py
@@ -5,10 +5,9 @@ from sqlalchemy import text
 from .api_master import api, db
 
 GenerateExcessReport_model = api.model('GenerateExcessReport',{"startdate": fields.DateTime(required=True)})
-GenerateProductUsage_model = api.model('GenerateProductUsage',{"startdate": fields.DateTime(required=True),
-                                                               "enddate": fields.DateTime(required=True)})
-GenerateSalesReport_model = api.model('GenerateSalesReport',{"startdate": fields.DateTime(required=True),
-                                                               "enddate": fields.DateTime(required=True)})
+GenerateProductUsage_model = api.model('GenerateProductUsage',{"startdate": fields.DateTime(required=True), "enddate": fields.DateTime(required=True)})
+GenerateSalesReport_model = api.model('GenerateSalesReport',{"startdate": fields.DateTime(required=True), "enddate": fields.DateTime(required=True)})
+GenerateOrderTrend_model = api.model('GenerateOrderTrend',{"startdate": fields.DateTime(required=True), "enddate": fields.DateTime(required=True)})
 
 @api.route('/api/manager/getmenuitems')
 class GetMenuItems(Resource):
@@ -57,7 +56,8 @@ class GenerateProductUsage(Resource):
             result = conn.execution_options(stream_results=True).execute(text(prod_usage_query))
             ingredientslist = []
             for row in result:
-                ingredientslist.append({"ingredientname":row.ingredientname, "totalamountchanged":row.totalamountchanged})
+                ingredientslist.append({"ingredientid":row.ingredientid,"ingredientname":row.ingredientname, "totalamountchanged":row.totalamountchanged})
+                #TODO: front end report will only need to show Ingredient Name and Total Amount Changed!
         return jsonify(ingredientslist)
     
 @api.route('/api/manager/reports/generatesalesreport')
@@ -68,9 +68,9 @@ class GenerateSalesReport(Resource):
         startdate_str = text(startdate)
         enddate = request.get_json().get("enddate") #TODO: PARAMETERIZE???
         enddate_str = text(enddate)
-        prod_usage_query = "SELECT menuitems.MenuID, menuitems.ItemName, SUM(menuitems.Price) AS TotalSales, COUNT(*) AS OrderCount FROM orders JOIN ordermenuitems ON orders.OrderID = ordermenuitems.OrderID JOIN menuitems ON ordermenuitems.MenuID = menuitems.MenuID WHERE orders.OrderDateTime BETWEEN TO_TIMESTAMP('{inputstart}', 'YYYY-MM-DD') AND TO_TIMESTAMP('{inputend}', 'YYYY-MM-DD') GROUP BY menuitems.MenuID, menuitems.ItemName ORDER BY TotalSales DESC".format(inputstart = startdate_str, inputend = enddate_str)
+        sales_query = "SELECT menuitems.MenuID, menuitems.ItemName, SUM(menuitems.Price) AS TotalSales, COUNT(*) AS OrderCount FROM orders JOIN ordermenuitems ON orders.OrderID = ordermenuitems.OrderID JOIN menuitems ON ordermenuitems.MenuID = menuitems.MenuID WHERE orders.OrderDateTime BETWEEN TO_TIMESTAMP('{inputstart}', 'YYYY-MM-DD') AND TO_TIMESTAMP('{inputend}', 'YYYY-MM-DD') GROUP BY menuitems.MenuID, menuitems.ItemName ORDER BY TotalSales DESC".format(inputstart = startdate_str, inputend = enddate_str)
         with db.engine.connect() as conn:
-            result = conn.execution_options(stream_results=True).execute(text(prod_usage_query))
+            result = conn.execution_options(stream_results=True).execute(text(sales_query))
             menuitemlist = []
             for row in result:
                 menuitemlist.append({"menuid":row.menuid, "itemname":row.itemname, "totalsales":row.totalsales, "ordercount":row.ordercount})
@@ -82,13 +82,41 @@ class GenerateExcessReport(Resource):
     def post(self): 
         startdate = request.get_json().get("startdate") #TODO: PARAMETERIZE???
         startdate_str = text(startdate)
-        excess_query = "SELECT DISTINCT mi.MenuID, mi.ItemName FROM ingredients i JOIN menuitemingredients mii ON i.ingredientid = mii.ingredientid JOIN menuitems mi ON mii.menuid = mi.menuid LEFT JOIN ( SELECT ingredientid, SUM(amountchanged) AS total_sold FROM InventoryLog WHERE amountchanged < 1 AND logdatetime BETWEEN CAST('{inputdate}' AS TIMESTAMP) AND NOW() GROUP BY ingredientid ) il ON i.ingredientid = il.ingredientid WHERE (il.total_sold IS NULL OR -1*il.total_sold < 0.1 * i.count);".format(inputdate = startdate_str)
+        excess_query = "SELECT DISTINCT mi.MenuID, mi.ItemName FROM ingredients i JOIN menuitemingredients mii ON i.ingredientid = mii.ingredientid JOIN menuitems mi ON mii.menuid = mi.menuid LEFT JOIN ( SELECT ingredientid, SUM(amountchanged) AS total_sold FROM InventoryLog WHERE amountchanged < 1 AND logdatetime BETWEEN CAST('{inputdate}' AS TIMESTAMP) AND NOW() GROUP BY ingredientid ) il ON i.ingredientid = il.ingredientid WHERE (il.total_sold IS NULL OR -1*il.total_sold < 0.1 * i.count)".format(inputdate = startdate_str)
         with db.engine.connect() as conn:
             result = conn.execution_options(stream_results=True).execute(text(excess_query))
             ingredientslist = []
             for row in result:
                 ingredientslist.append({"menuid":row.menuid, "itemname":row.itemname})
         return jsonify(ingredientslist)
+
+@api.route('/api/manager/reports/generaterestockreport')
+class GenerateRestockReport(Resource):
+    def get(self): 
+        restock_query = "SELECT * FROM ingredients WHERE count < minamount"
+        with db.engine.connect() as conn:
+            result = conn.execution_options(stream_results=True).execute(text(restock_query))
+            ingredientlist = []
+            for row in result:
+                ingredientlist.append({"ingredientid":row.ingredientid, "ingredientname":row.ingredientname, "ppu":row.ppu, "count":row.count, "minamount":row.minamount})
+                # TODO: front end will only need to show ingredientname, count as Current Amount, and minamount as Minimum Amount!
+        return jsonify(ingredientlist)
+
+@api.route('/api/manager/reports/generateordertrend')
+class GenerateOrderTrend(Resource):
+    @api.expect(GenerateOrderTrend_model, validate=True)
+    def post(self): 
+        startdate = request.get_json().get("startdate") #TODO: PARAMETERIZE???
+        startdate_str = text(startdate)
+        enddate = request.get_json().get("enddate") #TODO: PARAMETERIZE???
+        enddate_str = text(enddate)
+        order_trend_query = "SELECT DISTINCT MID1, MID2, Count (*) AS count FROM (SELECT n1.itemname AS MID1, n2.itemname AS MID2, t1.OrderID FROM OrderMenuItems t1 JOIN OrderMenuItems t2 ON t1.OrderID = t2.OrderID AND t1.menuID <  t2.MenuID JOIN Orders ON Orders.OrderID = t1.OrderID Join MenuItems n1 ON t1.MenuID = n1.MenuID JOIN MenuItems n2 on n2.MenuID = t2.MenuID WHERE Orders.OrderDateTime BETWEEN CAST('{inputstart}' AS DATE) AND CAST('{inputend}' AS DATE)) AS doubleJoin  GROUP BY MID1, MID2 ORDER BY count DESC LIMIT 10".format(inputstart = startdate_str, inputend = enddate_str)
+        with db.engine.connect() as conn:
+            result = conn.execution_options(stream_results=True).execute(text(order_trend_query))
+            menuitemlist = []
+            for row in result:
+                menuitemlist.append({"menuid1":row.mid1, "menuid2":row.mid2, "count":row.count})
+        return jsonify(menuitemlist)
 
 def init():
     return 
