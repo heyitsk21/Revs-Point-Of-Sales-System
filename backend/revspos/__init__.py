fcb7a66c2e2d5ed773428a01881f6003c6e50d12
jnuccitelli <jnuccitelli1@gmail.com>
2024-03-27 10:17:29 -0500

    added whitespace

--- backend/revspos/__init__.py
+++ backend/revspos/__init__.py
@@ -5,6 +5,8 @@ from .api_master import api
 from .employee_routes import init
 from .manager_routes import init
 
+
+
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='woah its a secret key') #TODO actually create a secret key 
