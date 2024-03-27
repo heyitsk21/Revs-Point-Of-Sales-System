3ef80624ed6701d4077969b481dd1b796f4149b7
heyitsk21 <122894491+heyitsk21@users.noreply.github.com>
2024-03-27 11:20:22 -0500

    excess report works! may need to parameterize

--- backend/revspos/manager_routes.py
+++ backend/revspos/manager_routes.py
@@ -4,6 +4,7 @@ from sqlalchemy import text
 # import os, decimal, datetime
 from .api_master import api, db
 
+GenerateExcessReport_model = api.model('GenerateExcessReport',{"startdate": fields.DateTime(required=True)})
 
 
 @api.route('/api/manager/getmenuitems')
@@ -36,6 +37,25 @@ class GetOrderHistory(Resource):
                 orderlist.append({"orderid":row.orderid, "customername":row.customername, "taxprice":row.taxprice,"baseprice":row.baseprice,"orderdatetime":row.orderdatetime,"employeeid":row.employeeid})
         return jsonify(orderlist)
     
-        
+
+
+
+
+
+
+@api.route('/api/manager/reports/generateexcessreport')
+class GenerateExcessReport(Resource):
+    @api.expect(GenerateExcessReport_model, validate=True)
+    def post(self): 
+        startdate = request.get_json().get("startdate")
+        startdate_str = text(startdate)
+        excess_query = "SELECT DISTINCT mi.MenuID, mi.ItemName FROM ingredients i JOIN menuitemingredients mii ON i.ingredientid = mii.ingredientid JOIN menuitems mi ON mii.menuid = mi.menuid LEFT JOIN ( SELECT ingredientid, SUM(amountchanged) AS total_sold FROM InventoryLog WHERE amountchanged < 1 AND logdatetime BETWEEN CAST('{inputdate}' AS TIMESTAMP) AND NOW() GROUP BY ingredientid ) il ON i.ingredientid = il.ingredientid WHERE (il.total_sold IS NULL OR -1*il.total_sold < 0.1 * i.count);".format(inputdate = startdate_str)
+        with db.engine.connect() as conn:
+            result = conn.execution_options(stream_results=True).execute(text(excess_query))
+            ingredientslist = []
+            for row in result:
+                ingredientslist.append({"menuid":row.menuid, "itemname":row.itemname})
+        return jsonify(ingredientslist)
+
 def init():
     return 
