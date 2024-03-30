d2eaa832a97d4f23fe22abcf9e4ab547a2b4a9f3
jnuccitelli <jnuccitelli1@gmail.com>
2024-03-29 20:46:38 -0500

    CORS

--- backend/revspos/__init__.py
+++ backend/revspos/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from .api_master import api
 from .employee_routes import init
 from .manager_routes import init
+from flask_cors import CORS
 
 
 
@@ -11,8 +12,10 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(SECRET_KEY='woah its a secret key') #TODO actually create a secret key 
   
+    cors = CORS()
     api.init_app(app)
-
+    print(cors)
+    cors.init_app(app)
     if test_config is None: 
         app.config.from_pyfile('config.py', silent=True)
     else:
