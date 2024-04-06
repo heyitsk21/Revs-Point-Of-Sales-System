5df270fa03f45aca22d7b9c48add00f8a59623d8
Seraphrai <84433119+Eragonlink@users.noreply.github.com>
2024-04-06 00:16:03 -0500

    Login page fixed, still need to globalize

--- frontend/src/LoginScreen.js
+++ frontend/src/LoginScreen.js
@@ -1,21 +1,21 @@
-import logo from './logo.csv'
 import { useEffect , useState } from 'react';
-import jwt_decode from "jwt-decode";
-import './Login.css'
+import { jwtDecode } from 'jwt-decode';
+import './LoginScreen.css'
 
-function Login(){
+function LoginScreen(){
+    //Change to global state or redux to manage login across components
     const [ user, setUser ] = useState({});
 
     function handleCallBackResponse(response) {
         console.log("Encoded JWT ID token: " + response.credential);
-        var userObject = jwt_decode(response.credential);
+        var userObject = jwtDecode(response.credential);
         console.log(userObject);
         setUser(userObject);
         document.getElementById("signInDiv").hidden = true;
     }
 
     function handleSignOut(event) {
-        setUser({});
+        setUser ({});
         document.getElementById("signInDiv").hidden = false;
     }
 
@@ -30,24 +30,24 @@ function Login(){
             document.getElementById("signInDiv"),
             { theme: "outline", size: "large"}
         );
-
-        google.accounts.id.prompt();
     }, []);
     // if no user show sign in
     // if user show logout
     return (
-        <div className='App'>
-            <div id="signInDiv">
-            { Object.keys(user).length != 0 && 
+        <div className='LoginScreen'>
+            <div id='signInDiv'></div>
+            { Object.keys(user).length != 0 &&
                 <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
             }
+            
             { user &&
                 <div>
                     <img src={user.picture}></img>
                     <h3>{user.name}</h3>
                 </div>
             }
-            </div>
         </div>
     );
 }
+
+export default LoginScreen;
