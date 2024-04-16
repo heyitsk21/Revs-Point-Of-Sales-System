69e4402c914f741fd3e6b8b6d43e32bf42a62158
Joshua Robert <joshdrobert@tamu.edu>
2024-04-16 12:23:58 -0500

    Updated Clock to be Military Time (for Google Translate)

--- frontend/src/components/ManagerTopBar.js
+++ frontend/src/components/ManagerTopBar.js
@@ -23,7 +23,10 @@ function ManagerTopBar(){
 
     const updateTime = () => {
         const date = new Date();
-        const timeString = date.toLocaleTimeString();
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        const seconds = date.getSeconds().toString().padStart(2, '0');
+        const timeString = `${hours}:${minutes}:${seconds}`;
         setCurrentTime(timeString);
     };
 
