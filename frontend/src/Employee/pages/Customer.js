bc204326ebcdb52ad385015f4b6a7c69bfa57548
Jessica Jakubik <123000032+jessjakubik@users.noreply.github.com>
2024-04-23 20:25:14 -0500

    customer fix

--- frontend/src/Employee/pages/Customer.js
+++ frontend/src/Employee/pages/Customer.js
@@ -3,13 +3,17 @@ import './EmpCust.css';
 import Ordering from '../components/Ordering/Ordering';
 import CustHeader from '../components/CustHeader/CustHeader'
 import { useTextSize } from '../../components/TextSizeContext';
+import { CartProvider } from 'react-use-cart';
+
 const Employee = ({ onCatChange }) => {
     const { textSize, toggleTextSize } = useTextSize();
     
     return (
         <div className={`employee ${textSize === 'large' ? 'large-text' : ''}`}>
-            <CustHeader/>
-            <Ordering/>
+            <CartProvider>
+                <CustHeader/>
+                <Ordering/>
+            </CartProvider>
         </div>
     );
 };
