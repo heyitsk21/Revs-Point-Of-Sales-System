90b6a3c08a505f2fe3ca396db8cc6b3391f4567b
Seraphrai <84433119+Eragonlink@users.noreply.github.com>
2024-05-01 13:12:29 -0500

    Employee management frontend complete

--- frontend/src/Manager/EmployeeManagement.js
+++ frontend/src/Manager/EmployeeManagement.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import './Inventory.css';
+import './EmployeeManagement.css';
 import './../Common.css';
 import { useTextSize } from '../components/TextSizeContext.js';
 import axios from 'axios';
 import ManagerTopBar from '../components/ManagerTopBar.js';
-import Restock from './Restock.js';
 
 function EmployeeManagement() {
     const [employees, setEmployees] = useState([]);
@@ -30,14 +29,13 @@ function EmployeeManagement() {
         }
     };
 
-    const handleItemSelected = (item) => {
+    const handleEmployeeSelected = (item) => {
         setSelectedEmployee(item);
     };
 
-    const handleItemUpdate = async () => {
+    const handleEmployeeUpdate = async () => {
         try {
             const payload = {
-                employeeid: parseInt(selectedEmployee.employeeid),
                 employeename: selectedEmployee.employeename,
                 email: selectedEmployee.email,
                 ismanager: selectedEmployee.ismanager,
@@ -45,47 +43,45 @@ function EmployeeManagement() {
                 password: selectedEmployee.password,
             };
     
-            await axios.put('https://team21revsbackend.onrender.com/api/manager/ingredients', payload);
-            alert('Ingredient edited successfully:');
+            await axios.put('https://team21revsbackend.onrender.com/api/manager/employee', payload);
+            alert('Employee edited successfully:');
             fetchEmployees();
         } catch (error) {
-            console.error('Error updating ingredient:', error);
+            console.error('Error updating Employee:', error);
         }
     };
 
-    const handleIngredientSubmit = async () => { //TODO
+    const handleEmployeeSubmit = async () => { //TODO
         try {
             const newEmployeeData = {
                 ...newEmployee,
-                count: parseInt(newEmployee.count),
-                ppu: parseFloat(newEmployee.ppu),
-                minamount: parseFloat(newEmployee.minamount),
-                recommendedamount: parseInt(newEmployee.recommendedamount),
-                caseamount: parseInt(newEmployee.caseamount)
+                employeename: newEmployee.employeename,
+                email: newEmployee.email,
+                ismanager: newEmployee.ismanager,
+                salary: parseFloat(newEmployee.salary),
+                password: newEmployee.password,
             };
     
-            const response = await axios.post('https://team21revsbackend.onrender.com/api/manager/ingredients', newEmployeeData);
-            alert('Ingredient added successfully:');
+            const response = await axios.post('https://team21revsbackend.onrender.com/api/manager/employee', newEmployeeData);
+            alert('Employee added successfully:');
             fetchEmployees();
             setNewEmployee({
-                ingredientid: 0,
-                ingredientname: "",
-                count: 0,
-                ppu: 0,
-                minamount: 0,
-                location: "",
-                recommendedamount: 0,
-                caseamount: 0
+                employeeid: 0,
+                employeename: "",
+                email: "",
+                ismanager: "False",
+                salary: 0.0,
+                password: "",
             });
         } catch (error) {
-            console.error('Error adding ingredient:', error);
+            console.error('Error adding Employee:', error);
         }
     };
 
-    const handleIngredientDelete = async (itemId, deleteCount) => {
-        if (window.confirm("Are you sure you want to delete this ingredient?")) {
+    const handleEmployeeDelete = async (deleteID) => {
+        if (window.confirm("Are you sure you want to delete this Employee?")) {
             try {
-                const response = await axios.delete('https://team21revsbackend.onrender.com/api/manager/ingredients', { data: { ingredientid: itemId, count: deleteCount } });
+                const response = await axios.delete('https://team21revsbackend.onrender.com/api/manager/employee', { data: { employeeid: deleteID } });
                 alert('Item deleted successfully:');
                 fetchEmployees();
             } catch (error) {
@@ -140,10 +136,10 @@ function EmployeeManagement() {
         }
     }, 1000);
 
-    const renderInventoryItems = () => {
+    const renderEmployeeItems = () => {
         return employees.map(employee => (
-            <div key={employee.ingredientid} className="inventory-item" onClick={() => handleItemSelected(employee)}>
-                <div className='ingredient-name'><span>{employee.employeename}</span></div>
+            <div key={employee.employeeid} className="employee-item" onClick={() => handleEmployeeSelected(employee)}>
+                <div className='employee-name'><span>{employee.employeename}</span></div>
                 <span>Employee Email: {employee.email}</span>
                 <span>Manager: { employee.ismanager ? 'Yes' : 'No' }</span>
                 <span>Salary: ${employee.salary}</span>
@@ -159,19 +155,14 @@ function EmployeeManagement() {
     return (
         <div>
             <ManagerTopBar />
-            <div className={`manager-inventory ${textSize === 'large' ? 'large-text' : ''}`}>
-                <div className="inventory-details">
+            <div className={`manager-employee ${textSize === 'large' ? 'large-text' : ''}`}>
+                <div className="employee-details">
                     <h2>Selected Employee Details</h2>
                     {selectedEmployee && (
-                        <div className="selected-item">
-                            <h3>{selectedEmployee.ingredientname}</h3>
+                        <div className="selected-employee">
+                            <h3>{selectedEmployee.employeename}</h3>
                             <div>
-                                <label>Employee ID:</label>
-                                <input
-                                    type="text"
-                                    value={selectedEmployee.employeeid}
-                                    onChange={(e) => handleInputChange(e, 'employeeid')}
-                                />
+                                <label>Employee ID: {selectedEmployee.employeeid}</label>
                             </div>
                             <div>
                                 <label>Name:</label>
@@ -213,22 +204,14 @@ function EmployeeManagement() {
                                     onChange={(e) => handleInputChange(e, 'password')}
                                 />
                             </div>
-                            <button className='ingredient-button' onClick={handleItemUpdate}>Submit</button>
-                            <button className='ingredient-button' onClick={() => handleIngredientDelete(selectedEmployee.employeeid)}>Delete</button>
+                            <button onClick={handleEmployeeUpdate}>Submit</button>
+                            <button onClick={() => handleEmployeeDelete(selectedEmployee.employeeid)}>Delete</button>
                         </div>
                     )}
                 </div>
-                <div className="new-ingredient">
+                <div className="new-employee">
                     <h2>Add New Employee</h2>
-                    <div className='new-ingredient-user'>
-                        <label>Employee ID:</label>
-                        <input
-                            type="number"
-                            value={newEmployee.employeeid}
-                            onChange={(e) => setNewEmployee({ ...newEmployee, employeeid: e.target.value })}
-                        />
-                    </div>
-                    <div className='new-ingredient-user'>
+                    <div className='new-employee-user'>
                         <label>Name:</label>
                         <input
                             type="text"
@@ -236,7 +219,7 @@ function EmployeeManagement() {
                             onChange={(e) => setNewEmployee({ ...newEmployee, employeename: e.target.value })}
                         />
                     </div>
-                    <div className='new-ingredient-user'>
+                    <div className='new-employee-user'>
                         <label>Email:</label>
                         <input
                             type="text"
@@ -244,7 +227,7 @@ function EmployeeManagement() {
                             onChange={(e) => setNewEmployee({ ...newEmployee, email: e.target.value })}
                         />
                     </div>
-                    <div className='new-ingredient-user'>
+                    <div className='new-employee-user'>
                         <label>Manager:</label>
                         <input
                             type="text"
@@ -252,7 +235,7 @@ function EmployeeManagement() {
                             onChange={(e) => setNewEmployee({ ...newEmployee, ismanager: e.target.value })}
                         />
                     </div>
-                    <div className='new-ingredient-user'>
+                    <div className='new-employee-user'>
                         <label>Salary:</label>
                         <input
                             type="number"
@@ -260,20 +243,20 @@ function EmployeeManagement() {
                             onChange={(e) => setNewEmployee({ ...newEmployee, salary: e.target.value })}
                         />
                     </div>
-                    <div className='new-ingredient-user'>
+                    <div className='new-employee-user'>
                         <label>Password:</label>
                         <input
-                            type="number"
+                            type="text"
                             value={newEmployee.password}
                             onChange={(e) => setNewEmployee({ ...newEmployee, password: e.target.value })}
                         />
                     </div>
-                    <button className='ingredient-button' onClick={handleIngredientSubmit}>Submit</button>
+                    <button onClick={handleEmployeeSubmit}>Submit</button>
                 </div>
 
-                <div className="inventory-list">
+                <div className="employee-list">
                     <h2>Employees</h2>
-                    {renderInventoryItems()}
+                    {renderEmployeeItems()}
                 </div>
             </div>
         </div>
