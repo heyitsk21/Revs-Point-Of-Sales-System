63555f07d319b86621cc64dd3d2febc2585b65cb
Joshua Robert <joshdrobert@tamu.edu>
2024-05-01 12:09:14 -0500

    Customer High Contrast

--- frontend/src/Employee/pages/Customer.js
+++ frontend/src/Employee/pages/Customer.js
@@ -1,18 +1,23 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState } from 'react';
 import './Customer.css';
 import CustOrdering from '../components/Ordering/CustOrdering';
-import CustHeader from '../components/CustHeader/CustHeader'
+import CustHeader from '../components/CustHeader/CustHeader';
 import { useTextSize } from '../../components/TextSizeContext';
 import { CartProvider } from 'react-use-cart';
 
 const Employee = ({ onCatChange }) => {
     const { textSize, toggleTextSize } = useTextSize();
-    
+    const [highContrast, setHighContrast] = useState(false);
+
+    const toggleHighContrast = () => {
+        setHighContrast(prevState => !prevState);
+    };
+
     return (
-        <div className={`employee ${textSize === 'large' ? 'large-text' : ''}`}>
+        <div className={`employee ${textSize === 'large' ? 'large-text' : ''} ${highContrast ? 'high-contrast' : ''}`}>
             <CartProvider>
-                <CustHeader/>
-                <CustOrdering/>
+                <CustHeader toggleHighContrast={toggleHighContrast} />
+                <CustOrdering />
             </CartProvider>
         </div>
     );
